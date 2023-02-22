76e38824dcb28e7b1e2f81df25e8002f81b2d748
Juan Cisilino <54241344+JuanCisilino@users.noreply.github.com>
2023-02-22 18:55:22 -0300

    logica de creacion de usuario por endpoint

--- api/views.py
+++ api/views.py
@@ -65,7 +65,10 @@ class VademecumGenericoListCreate(generics.ListCreateAPIView):
 
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -73,4 +76,14 @@ def home(request):
 def token(request):
     user = User.objects.create_user('token_login', 'email@example.com', 'ribo1234')
     user.save()
-    return render(request, user)
+    auth_user = authenticate(username='token_login', password='ribo1234')
+    if auth_user:
+            # Crear un token JWT
+        refresh = RefreshToken.for_user(auth_user)
+        return Response({
+                'message': 'Usuario creado con éxito',
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh)
+            }, status=status.HTTP_201_CREATED)
+    else:
+        return Response({'message': 'No se pudo autenticar al usuario'}, status=status.HTTP_400_BAD_REQUEST)
